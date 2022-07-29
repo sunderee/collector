8108db46830beff5cf46e02f7370caa0f34c5dc3
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2022-07-29 09:54:40 +0200

    Make the app deployable

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+flutter clean && flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter build appbundle \
+    --obfuscate \
+    --split-debug-info=symbols

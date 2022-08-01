e09baf301e71e23e2b3f70f903b6fdc8ce318fd2
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2022-08-01 07:58:17 +0200

    Ensure WidgetsBinding is initialized before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   Intl.systemLocale = await findSystemLocale();
   await initializeDependencies();
 

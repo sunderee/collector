20157673f37d6028768f7d9d537e305d1756818a
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2022-08-27 08:11:55 +0200

    Introduce Isar

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,14 @@
 import 'package:get_it/get_it.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 final GetIt getIt = GetIt.instance;
 
-Future<void> initializeDependencies() async {}
+Future<void> initializeDependencies() async {
+  final directory = await getApplicationDocumentsDirectory();
+  final isarInstance = await Isar.open(
+    schemas: [],
+    directory: directory.path,
+  );
+  getIt.registerSingleton<Isar>(isarInstance);
+}
